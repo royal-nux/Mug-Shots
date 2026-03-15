8e33dfca984eb747fbfa87926fde9aa2d35a354e
royal-nux <moloi12kat@gmail.com>
2026-03-15 15:48:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function adminLogin() {
     const username = document.getElementById('admin-username').value;
     const password = document.getElementById('admin-password').value;
     
-    if (username === 'admin mg' && password === 'dijejeablo') {
+    if (username === 'admin' && password === 'dijejeablo') {
         closeAdminLogin();
         loadAdminDashboard();
         document.getElementById('admin-dashboard').style.display = 'flex';
